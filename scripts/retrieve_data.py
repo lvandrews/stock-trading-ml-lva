848d4c0e35e82634771b5dd792d53ae579b42128
lvandrews <lelavioletandrews@gmail.com>
2022-10-03 20:22:33 -0700

    working for daily and intraday data

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -8,25 +8,18 @@ desctext = 'retrieve_data.py: use AlphaVantage stock API to pull stock data and 
 vers='retrieve_data.py v0.1'
 
 # Parse inputs or provide help
-import argparse, sys, json, time, os
+import argparse, sys, json, time, os, pprint
 import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
-#import json
-#import time
-#import os
 
-# Define date strings
-#today=dt.date.today()
-#year_ago=today - dt.timedelta(days=365)
-#date_now = time.strftime("%Y-%m-%d_%H-%M-%S")
+# Define date string, get script working directory
 date_now_notime = time.strftime("%Y-%m-%d")
-#dt_string = time.strftime("%b-%d-%Y %I:%M:%S %p")
+workdir = os.getcwd()
 
 # Initialize parser
 parser = argparse.ArgumentParser(description=desctext)
 parser.add_argument("-t", "--ticker", help="Ticker abbreviation (e.g. AMZN, required)", type=str, metavar="", required=True)
-parser.add_argument("-d", "--data_type", help="Data type to retrieve (daily, daily_adj, or intraday; default=daily)", choices=["daily", "adj_daily", "intraday"], type=str, metavar="", default="daily")
+parser.add_argument("-d", "--data_type", help="Data type to retrieve (daily, daily_adj (p), intraday or intraday_ext; default=daily)", choices=["daily", "adj_daily", "intraday", "intraday_ext"], type=str, metavar="", default="daily")
 parser.add_argument("-v", "--version", help="show program version", action="version", version=vers)
 parser.add_argument("-V", "--verbose", help="increase output verbosity", action="store_true")
 
@@ -41,47 +34,84 @@ if args.verbose:
     print("Verbosity turned on")
 
 # Parse inputs and set ticker to uppercase if lowercase was entered
-#parser.parse_args()
 ticker = args.ticker.upper()
-#data_type = args.data_type()
-#print(ticker)
-#print(args.data_type)
 dtype = args.data_type
-#print(dtype)
-#print(data_type)
-#print(vars(args))
-
+ticker_data_filename = os.path.join(f"../data/{ticker}_{date_now_notime}_{dtype}.csv")
+#symbol = ticker
+time_window = dtype
 
 
 #quit()
 # Print important model parameters
 print("Ticker symbol:", ticker)
-
 print("Data type:", dtype)
-#print("Epochs to train:", args.epoch)
-#print("Test size:", args.test_size)
-#print("Window size:", args.window_size)
-#print("Lookup step:", args.lookup_step)
-#print("RNN cell: LSTM")
 
-# Ticker data filename
-ticker_data_filename = os.path.join(f"{ticker}_{date_now_notime}_{dtype}.csv")
-
-print("Output file here:", ticker_data_filename)
-def save_dataset(ticker, dtype):
+# Retrieve data function
+def save_dataset(symbol, time_window):
     credentials = json.load(open('creds.json', 'r'))
     api_key = credentials['av_api_key']
+    print(symbol, time_window)
     ts = TimeSeries(key=api_key, output_format='pandas')
-    if dtype == 'intraday':
-        data, meta_data = ts.get_intraday(ticker, interval='1min', outputsize='full')
-    elif dtype == 'daily':
-       data, meta_data = ts.get_daily(ticker, outputsize='full')
-    elif dtype == 'daily_adj':
-        data, meta_data = ts.get_daily_adjusted(ticker, outputsize='full')
-
-    pprint(data.head(10))
+    if time_window == 'intraday':
+        data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='full')
+    elif time_window == 'daily':
+        data, meta_data = ts.get_daily(symbol, outputsize='full')
+    elif time_window == 'adj_daily':
+        data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+    elif time_window == 'intraday_ext':
+        data, meta_data = ts.get_time_series_intraday_extended(symbol, interval='15min')
 
     data.to_csv(ticker_data_filename)
+#    pprint(data.head(10))
+
+save_dataset(ticker, dtype)
+#    save_dataset(**vars(namespace))
+
+#    data.to_csv(ticker_data_filename)
+#    data, meta_data = ts.get_daily(symbol, outputsize='full')
+#    data.to_csv(ticker_data_filename)
+
+# Execute function
+#save_dataset(ticker, dtype)
+
+
+#def save_dataset(symbol, time_window):
+#    credentials = json.load(open('creds.json', 'r'))
+#    api_key = credentials['av_api_key']
+#    print(symbol, time_window)
+#    ts = TimeSeries(key=api_key, output_format='pandas')
+#    if time_window == 'intraday':
+#        data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='full')
+#    elif time_window == 'daily':
+#        data, meta_data = ts.get_daily(symbol, outputsize='full')
+#    elif time_window == 'daily_adj':
+#        data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+#
+#    pprint(data.head(10))
+#
+#    data.to_csv(ticker_data_filename)
+#
+#    save_dataset(**vars(namespace))
+
+
+
+
+
+#print("Output file here:", ticker_data_filename)
+#def save_dataset(ticker, dtype):
+#    credentials = json.load(open('creds.json', 'r'))
+#    api_key = credentials['av_api_key']
+#    ts = TimeSeries(key=api_key, output_format='pandas')
+#    if dtype == 'intraday':
+#        data, meta_data = ts.get_intraday(ticker, interval='1min', outputsize='full')
+#    elif dtype == 'daily':
+#       data, meta_data = ts.get_daily(ticker, outputsize='full')
+#    elif dtype == 'daily_adj':
+#        data, meta_data = ts.get_daily_adjusted(ticker, outputsize='full')
+#
+#    pprint(data.head(10))
+#
+#    data.to_csv(ticker_data_filename)
 
 
 #if __name__ == "__main__":
@@ -91,6 +121,6 @@ def save_dataset(ticker, dtype):
 #    parser.add_argument('time_window', type=str, choices=[
 #                        'intraday', 'daily', 'daily_adj'], help="the time period you want to download the stock history for")
 
-    namespace = parser.parse_args()
-    save_dataset(**vars(namespace))
+#    namespace = parser.parse_args()
+#    save_dataset(**vars(namespace))
     
