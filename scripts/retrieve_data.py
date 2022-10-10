aba3ceb219442140f1106b583ae4bf5a69d6a561
lvandrews <lelavioletandrews@gmail.com>
2022-10-09 21:13:46 -0700

    added code for yahoo_fin, other options

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -12,6 +12,7 @@ import argparse, sys, json, time, os, pprint
 import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
 from os.path import dirname, abspath
+from yahoo_fin import stock_info as si
 
 # Define date string, repo directories, credentials
 date_now_notime = time.strftime("%Y-%m-%d")
@@ -25,6 +26,8 @@ av_creds = os.path.join(scriptdir,"creds.json")
 parser = argparse.ArgumentParser(description=desctext)
 parser.add_argument("-t", "--ticker", help="Ticker abbreviation (e.g. AMZN, required)", type=str, metavar="", required=True)
 parser.add_argument("-d", "--data_type", help="Data type to retrieve (daily, daily_adj (p), intraday or intraday_ext; default=daily)", choices=["daily", "adj_daily", "intraday", "intraday_ext"], type=str, metavar="", default="daily")
+parser.add_argument("-n", "--interval", help="Time interval between data points (intraday only; 1min, 5min, 15min, 30min, 60min); default=5min)", choices=["1min", "5min", "15min", "30min", "60min"], type=str, metavar="", default="5min")
+parser.add_argument("-s", "--source", help="Select data source; default=AlphaVantage", choices=["alphavantage", "yahoo"], type=str, metavar="", default="alphavantage")
 parser.add_argument("-v", "--version", help="show program version", action="version", version=vers)
 parser.add_argument("-V", "--verbose", help="increase output verbosity", action="store_true")
 
@@ -43,27 +46,61 @@ ticker = args.ticker.upper()
 dtype = args.data_type
 ticker_data_filename = os.path.join(datadir,f"{ticker}_{date_now_notime}_{dtype}.csv")
 time_window = dtype
+source = args.source
+intvl = args.interval
 
 # Print important model parameters
 print("Ticker symbol:", ticker)
 print("Data type:", dtype)
+if dtype == "intraday":
+    print("Time interval:", intvl)
+if source == "alphavantage":
+    print("API source: AlphaVantage")
+elif source == "yahoo":
+    print("API source: Yahoo Finance")
+    
+# Retrieve data function AlphaVantage
+if source == "alphavantage":
+    def save_dataset(symbol, time_window):
+        credentials = json.load(open(av_creds, 'r'))
+        api_key = credentials['av_api_key']
+        ts = TimeSeries(key=api_key, output_format='pandas')
+        if time_window == 'intraday':
+            data, meta_data = ts.get_intraday(symbol, interval=intvl, outputsize='full')
+        elif time_window == 'daily':
+            data, meta_data = ts.get_daily(symbol, outputsize='full')
+        elif time_window == 'adj_daily':
+            print("\nadj_daily data disabled due to paywall at AlphaVantage\n ----- EXITING -----\n")
+            quit()
+            #data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+        elif time_window == 'intraday_ext':
+            print("\nintraday_ext data disabled\n ----- EXITING -----\n")
+            quit()
+        #    data, meta_data = ts.get_intraday_extended(symbol, interval='15min')
 
-# Retrieve data function
-def save_dataset(symbol, time_window):
-    credentials = json.load(open(av_creds, 'r'))
-    api_key = credentials['av_api_key']
-    print(symbol, time_window)
-    ts = TimeSeries(key=api_key, output_format='pandas')
-    if time_window == 'intraday':
-        data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='full')
-    elif time_window == 'daily':
-        data, meta_data = ts.get_daily(symbol, outputsize='full')
-    elif time_window == 'adj_daily':
-        data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
-    elif time_window == 'intraday_ext':
-        data, meta_data = ts.get_time_series_intraday_extended(symbol, interval='15min')
+        data.to_csv(ticker_data_filename)
+        
+    save_dataset(ticker, dtype)
 
-    data.to_csv(ticker_data_filename)
+# Retrieve data function Yahoo
+if source == "yahoo":
+    print("\nYahoo finance calls disabled at this time\n ----- EXITING -----\n")
+    quit()
+#    def save_dataset(symbol, time_window):
+#        credentials = json.load(open(av_creds, 'r'))
+#        api_key = credentials['av_api_key']
+#        print(symbol, time_window)
+#        ts = TimeSeries(key=api_key, output_format='pandas')
+#        if time_window == 'intraday':
+#            data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='full')
+#        elif time_window == 'daily':
+#            data, meta_data = ts.get_daily(symbol, outputsize='full')
+#        elif time_window == 'adj_daily':
+#            data, meta_data = ts.get_daily_adjusted(symbol, outputsize='full')
+#        elif time_window == 'intraday_ext':
+#            data, meta_data = ts.get_time_series_intraday_extended(symbol, interval='15min')
+#
+#       data.to_csv(ticker_data_filename)
 
-save_dataset(ticker, dtype)
+#    save_dataset(ticker, dtype)
     
